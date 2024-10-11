5d4647e058715045ac0fdd9311a22c10148cf664
Ariznawl@163.com <ariznawl@163.com>
2024-10-11 14:06:06 +0800

    test pitr

--- pkg/vm/engine/tae/options/types.go
+++ pkg/vm/engine/tae/options/types.go
@@ -44,7 +44,8 @@ const (
 	DefaultCheckpointMinCount           = int64(100)
 	DefaultCheckpointIncremetalInterval = time.Minute
 	DefaultCheckpointGlobalMinCount     = 10
-	DefaultGlobalVersionInterval        = time.Hour
+        DefaultGlobalVersionInterval        = 5 * time.Minute
+//	DefaultGlobalVersionInterval        = time.Hour
 	DefaultGCCheckpointInterval         = time.Minute
 	DefaultOverallFlushMemControl       = common.Const1GBytes
 
